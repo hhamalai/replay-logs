bfb3fc544d3d92ccd5176fabafb5c5db2e8dffb8
Harri Hämäläinen <hhamalai@iki.fi>
2020-04-25 12:43:25 +0300

    Init

---
+++ README.md
@@ -0,0 +1,25 @@
+# replay-logs
+
+To transfer AWS CloudWatch log streams to AWS Kinesis Data Streams one can create AWS CloudWatch logs subscription, that
+will take care of exporting the log streams into Kinesis. It might happen that, e.g. due to malfunction of the system
+reading the Kinesis Data Streams, some log streams need to be migrated later again. If the defined Kinesis Data Stream
+retention period is expired, the logs must be resent to Kinesis from CloudWatch logs manually.
+
+This application will do exactly that. Given log event selectors like start/end timestamps and log filter pattern, the 
+application query the CloudWatch logs and puts all returned log events as Kinesis Data Stream records into defined
+stream, as they would have published by CloudWatch logs subscription.
+
+### Usage
+```
+# Transfer logs from a time window into Kinesis stream called "LogDataStream"
+$ ./replay-logs -start-time "2020-04-24T16:35:05" -end-time 2020-04-24T23:59:59 -kinesis-stream LogDataStream
+
+# Transfer logs from starting from start-time timestamp and matching to filter-pattern
+# into Kinesis Stream called "LogDataStream"
+$ ./replay-logs \
+  -start-time "2020-04-24T16:35:05" \
+  -end-time 2020-04-24T23:59:59 \
+  -filter-pattern '{ $.kubernetes.labels.app = "my-application" }' \ 
+  -kinesis-stream LogDataStream
+```
+
---
+++ main.go
@@ -0,0 +1,133 @@
+package main
+
+import (
+	"bytes"
+	"compress/gzip"
+	"flag"
+	"fmt"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/cloudwatchlogs"
+	"github.com/aws/aws-sdk-go/service/kinesis"
+	"os"
+	"time"
+)
+
+const DatetimeFormat = "2006-01-02T15:04:05"
+const LogSliceMaxCapacity = 500
+
+func parseDateTime(t0 *string) (*time.Time, error) {
+	parsed, err := time.Parse(DatetimeFormat, *t0)
+	if err != nil {
+		return nil, err
+	}
+	return &parsed, nil
+}
+
+func main() {
+	region := flag.String("region", os.Getenv("AWS_REGION"), "AWS region where logs are stored")
+	logGroup := flag.String("log-group", "/eks/EKS-Cluster/containers", "AWS CloudWatch Log Group where logs are stored")
+	sTime := flag.String("start-time", "", "Starting time for the time window from which the logs are replayed")
+	eTime := flag.String("end-time", "", "Ending time for the time window from which the logs are replayed")
+	kinesisStream := flag.String("kinesis-stream", "LogDataStream", "Name of Kinesis stream the logs are transferred to")
+	filterPattern := flag.String("filter-pattern", "\"*\"", "CloudWatch logs filter pattern to select exported logs")
+	flag.Parse()
+
+	if *sTime == "" {
+		fmt.Println("Start time must be set with command line argument")
+		return
+	}
+
+	var startTime *int64 = nil
+	e0, err := parseDateTime(sTime)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	startTime = aws.Int64(e0.Unix() * 1000)
+
+	var endTime *int64 = nil
+	if *eTime != "" {
+		e0, err := parseDateTime(eTime)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		endTime = aws.Int64(e0.Unix() * 1000)
+	}
+
+	fmt.Println("Filtering log group and forwarding filtered events to kinesis stream")
+	fmt.Printf("\tLog group: %s\n", *logGroup)
+	fmt.Printf("\tKinesis stream name: %s\n", *kinesisStream)
+	fmt.Printf("\tFilter pattern: %s\n", *filterPattern)
+	fmt.Printf("\tRegion: %s\n", *region)
+	fmt.Printf("\tStart time: %s (%d)\n", *sTime, *startTime)
+
+	if endTime != nil {
+		fmt.Printf("\tEnd time: %s (%d) \n", *eTime, *endTime)
+	}
+
+	mySession := session.Must(session.NewSession())
+	config := aws.NewConfig().WithRegion(aws.StringValue(region))
+
+	cloudwatchSvc := cloudwatchlogs.New(mySession, config)
+	kinesisSvc := kinesis.New(mySession, config)
+
+
+	fmt.Println("################ Starting replay ################")
+	err = cloudwatchSvc.FilterLogEventsPages(&cloudwatchlogs.FilterLogEventsInput{
+		LogGroupName:  logGroup,
+		FilterPattern: filterPattern,
+		StartTime:     startTime,
+		EndTime:       endTime,
+	}, func(filteredEvents *cloudwatchlogs.FilterLogEventsOutput, lastPage bool) bool {
+		events := filteredEvents.Events
+		eventsLen := len(events)
+		if eventsLen == 0 {
+			return true
+		}
+		for j := 0; j <= eventsLen/LogSliceMaxCapacity; j++ {
+			var records []*kinesis.PutRecordsRequestEntry
+			var sliceEnd = j*LogSliceMaxCapacity + LogSliceMaxCapacity
+			if sliceEnd > eventsLen {
+				sliceEnd = eventsLen
+			}
+			var buf bytes.Buffer
+			for i, event := range filteredEvents.Events[j*LogSliceMaxCapacity : sliceEnd] {
+				buf.Reset()
+				com := gzip.NewWriter(&buf)
+				_, err := com.Write([]byte(*event.Message))
+				if err != nil {
+					fmt.Println(err)
+					return false
+				}
+				err = com.Close()
+				if err != nil {
+					fmt.Println(err)
+					return false
+				}
+				if i == 0 {
+					fmt.Println("Processing messages from", time.Unix(*event.Timestamp/1000, 0))
+				}
+				records = append(records, &kinesis.PutRecordsRequestEntry{
+					Data:         buf.Bytes(),
+					PartitionKey: event.LogStreamName,
+				})
+			}
+
+			_, err := kinesisSvc.PutRecords(&kinesis.PutRecordsInput{
+				Records:    records,
+				StreamName: kinesisStream,
+			})
+			if err != nil {
+				fmt.Printf("Failed to publish logs to Kinesis: %v\n", err)
+				return false
+			}
+		}
+		return true
+	})
+
+	if err != nil {
+		fmt.Printf("Log replay failed: %s\n", err)
+	}
+}
